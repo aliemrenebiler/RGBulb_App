07b509cd0862e79f73fb67b9af590554e9723197
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2022-05-13 11:53:52 +0300

    project was created

---
+++ lib/screens/connectscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ConnectScreen extends StatefulWidget {
+  const ConnectScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ConnectScreen> createState() => _ConnectScreenState();
+}
+
+class _ConnectScreenState extends State<ConnectScreen> {
+  refresh() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/controlscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ControlScreen extends StatefulWidget {
+  const ControlScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ControlScreen> createState() => _ControlScreenState();
+}
+
+class _ControlScreenState extends State<ControlScreen> {
+  refresh() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
